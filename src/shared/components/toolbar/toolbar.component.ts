86f4cc8066f607ddced0e412add784dcf6d15772
Raizo400 <lgustropa@gmail.com>
2025-05-15 17:29:30 -0500

    feat toolbar.component.ts

--- src/shared/components/toolbar/toolbar.component.ts
+++ src/shared/components/toolbar/toolbar.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {LanguageSwitcherComponent} from '../../../public/components/language-switcher/language-switcher.component';
 
 @Component({
   selector: 'toolbar-app',
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, LanguageSwitcherComponent],
 })
 export class Toolbar {}
