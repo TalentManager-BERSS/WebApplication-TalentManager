caca1ec1758d803c5f978e83e2769b9dd2c754ae
S.Suarez <166082221+SDarXx@users.noreply.github.com>
2025-05-15 16:05:11 -0500

    Feat app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import {Component} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatAnchor} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
+  imports: [
+    MatSidenavModule,
+    MatAnchor,
+    RouterLink,
+    RouterOutlet,
+    RouterLinkActive,
+    TranslatePipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'catch-up';
+
+  options = [
+    {link: '/home', label: 'Home', icon: 'home'},
+    {link: '/reports', label: 'Reports', icon: 'insert_chart'},
+    {link: '/dashboards', label: 'Dashboards', icon: 'dashboard'},
+    {link: '/support', label: 'Support', icon: 'headset_mic'},
+    {link: '/logout', label: 'Logout', icon: 'logout'},
+  ]
+
+
   constructor(private translate: TranslateService) {
     this.translate.addLangs(['en', 'es']);
     this.translate.setDefaultLang('en');
