6863cce6415256ea2f74042c8d3b2c1eda4d172e
S.Suarez <166082221+SDarXx@users.noreply.github.com>
2025-05-15 16:07:41 -0500

    Feat app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+//Agregar aqui los componentes
+export const routes: Routes = [
+  { path: 'home',             component: '' },
+  { path: 'reports',             component: '' },
+  { path: 'dashboards',             component: '' },
+  { path: 'support',             component: '' },
+  { path: 'logout',             component: '' },
 
-export const routes: Routes = [];
+];
+
