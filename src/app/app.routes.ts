b60c1c4e91f3ea0576b320f746be8841430e82e9
Raizo400 <lgustropa@gmail.com>
2025-05-16 03:49:10 -0500

    Feat: Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import {CourseManagementComponent} from './Employees/pages/Home/components/home.component';
 import {SupportComponent} from './Employees/pages/Support/components/support.component'
 import {ReportsComponent} from './Employees/pages/Reports/components/reports.component';
+import {HomeComponent} from './Employees/pages/Home/components/home.component';
 
 //Agregar aqui los componentes
 export const routes: Routes = [
-  { path: 'home',             component: CourseManagementComponent },
+  { path: 'home',             component: HomeComponent },
   { path: 'reports',             component: ReportsComponent },
   //{ path: 'dashboards',             component: '' },
   { path: 'support',             component: SupportComponent },
