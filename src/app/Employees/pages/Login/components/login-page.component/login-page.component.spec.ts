42f367518256efcddcee237b37be00c45f5f0a1c
Gabriel Borja <borjagabriel45@gmail.com>
2025-05-16 05:33:10 -0500

    Update login-page.component.spec.ts

--- src/app/Employees/pages/Login/components/login-page.component/login-page.component.spec.ts
+++ src/app/Employees/pages/Login/components/login-page.component/login-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginPage } from './login-page.component.component';
+import { LoginPage } from './login-page.component';
 
 describe('LoginPageComponentComponent', () => {
   let component: LoginPage;
