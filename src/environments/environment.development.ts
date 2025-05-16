4b83bd95dc6aa84ee27999a24c8687bd00cccedc
SantiagoCárdenas <166081997+Sack136@users.noreply.github.com>
2025-05-16 11:56:29 -0500

    Fix: Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -7,7 +7,7 @@ export const environment = {
   // serverBaseURL: 'http://localhost:8080/api/v1'
   serverBaseUrl: 'https://681fc1f272e59f922ef71049.mockapi.io/api/v1',
   reportsserverBaseUrl: 'https://6824f9c70f0188d7e72b99b7.mockapi.io',
-  coursesEndpointPath: '/courses',
+  coursesEndpointPath: '/employee',
 
 
   reportsEndpointPath: '/reports',
