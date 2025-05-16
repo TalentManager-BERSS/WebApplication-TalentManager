d60cf8345f6f39ba76f97bec8a0e4a82964af47a
Raizo400 <lgustropa@gmail.com>
2025-05-16 06:42:06 -0500

    Feat: update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,5 +6,9 @@ export const environment = {
   // Server Base URL for Spring Boot REST API
   // serverBaseURL: 'http://localhost:8080/api/v1'
   serverBaseUrl: 'https://681fc1f272e59f922ef71049.mockapi.io/api/v1',
-  coursesEndpointPath: '/employee',
+  reportsserverBaseUrl: 'https://6824f9c70f0188d7e72b99b7.mockapi.io',
+  coursesEndpointPath: '/courses',
+
+
+  reportsEndpointPath: '/reports',
 };
