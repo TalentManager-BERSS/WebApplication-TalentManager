bbc84fcf6ea9379e0d5c13f1f96e154d52fddbb6
S.Suarez <166082221+SDarXx@users.noreply.github.com>
2025-05-16 01:15:43 -0500

    Feat: update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import {CourseManagementComponent} from './Employees/pages/Home/components/home.component';
+import {SupportComponent} from './Employees/pages/Support/components/support.component'
+
 //Agregar aqui los componentes
 export const routes: Routes = [
   { path: 'home',             component: CourseManagementComponent },
   //{ path: 'reports',             component: '' },
   //{ path: 'dashboards',             component: '' },
-  //{ path: 'support',             component: '' },
+  { path: 'support',             component: SupportComponent },
   //{ path: 'logout',             component: '' },
 
 ];
