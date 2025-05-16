7d00c82ab8a8a31d25100fd4812e7ab6fb552524
S.Suarez <166082221+SDarXx@users.noreply.github.com>
2025-05-16 05:59:14 -0500

    Fix: app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import {SupportComponent} from './Employees/pages/Support/components/support.component'
 import {ReportsComponent} from './Employees/pages/Reports/components/reports.component';
 import {HomeComponent} from './Employees/pages/Home/components/home.component';
+import {ListEmployeeComponent} from './Employees/pages/Employees/components/List-employee/list-employee.component';
+import {EmployeeAddComponent} from './Employees/pages/Employees/components/Add-Employee/add-employee.component';
 
 //Agregar aqui los componentes
 export const routes: Routes = [
@@ -10,6 +12,9 @@ export const routes: Routes = [
   //{ path: 'dashboards',             component: '' },
   { path: 'support',             component: SupportComponent },
   //{ path: 'logout',             component: '' },
+  { path: 'add',             component:  EmployeeAddComponent},
+  { path: 'manage',             component:  ListEmployeeComponent},
+
 
 ];
 
