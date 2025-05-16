61930aeec7933980d738daa32335f5987bfe4c36
SantiagoCárdenas <166081997+Sack136@users.noreply.github.com>
2025-05-16 11:56:45 -0500

    Fix: Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
   // Server Base URL for REST API
   serverBaseUrl: 'https://681fc1f272e59f922ef71049.mockapi.io/api/v1',
-  coursesEndpointPath: '/courses',
+  coursesEndpointPath: '/employee',
   reportsserverBaseUrl: 'https://6824f9c70f0188d7e72b99b7.mockapi.io',
   reportsEndpointPath: '/reports',
 
