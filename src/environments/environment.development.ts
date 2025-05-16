8306b148e3b23970f7e0062e8228507b4faa4d4d
SantiagoCárdenas <166081997+Sack136@users.noreply.github.com>
2025-05-16 00:16:26 -0500

    Fix: Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,10 @@
 export const environment = {
+  // In real lifecycles, this file should not be part of version control
   production: false,
-  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
-  newsProviderNewsEndpointPath: '/top-headlines',
-  newsProviderSourcesEndpointPath: '/sources',
-  newsProviderApiKey: '0d5b87d6eed74a768b7f2f7a3ca1bafb',
-  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
+  // Server Base URL for Fake REST API
+  // serverBaseURL: 'http://localhost:3000/api/v1'
+  // Server Base URL for Spring Boot REST API
+  // serverBaseURL: 'http://localhost:8080/api/v1'
+  serverBaseUrl: 'https://681fc1f272e59f922ef71049.mockapi.io/api/v1',
+  coursesEndpointPath: '/courses',
 };
