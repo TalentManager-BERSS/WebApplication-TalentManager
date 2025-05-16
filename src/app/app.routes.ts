830d0b2fd5c5934a76a3c7f2c667ac5498781803
SantiagoCárdenas <166081997+Sack136@users.noreply.github.com>
2025-05-16 02:11:19 -0500

    Fix: Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import {CourseManagementComponent} from './Employees/pages/Home/components/home.component';
+import {HomeComponent} from './Employees/pages/Home/components/home.component';
 import {SupportComponent} from './Employees/pages/Support/components/support.component'
 
 //Agregar aqui los componentes
 export const routes: Routes = [
-  { path: 'home',             component: CourseManagementComponent },
+  { path: 'home',             component: HomeComponent },
   //{ path: 'reports',             component: '' },
   //{ path: 'dashboards',             component: '' },
   { path: 'support',             component: SupportComponent },
