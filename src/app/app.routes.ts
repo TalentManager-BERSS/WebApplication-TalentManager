16712c724b076283eb4fee762961b884cb4685bc
SantiagoCárdenas <166081997+Sack136@users.noreply.github.com>
2025-05-16 14:16:01 -0500

    Fix: Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,21 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideTranslateService, TranslateLoader} from '@ngx-translate/core';
-import {HttpClient, provideHttpClient} from '@angular/common/http';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
-import {provideRouter} from '@angular/router';
-import {routes} from './app.routes';
+import { Routes } from '@angular/router';
+import {SupportComponent} from './Employees/pages/Support/components/support.component'
+import {ReportsComponent} from './Employees/pages/Reports/components/reports.component';
+import {HomeComponent} from './Employees/pages/Home/components/home.component';
+import {LoginPage} from './Employees/pages/Login/components/login-page.component/login-page.component';
+import {DashboardMain} from './Employees/pages/Dashboard/components/dashboard-main.component/dashboard-main.component';
+import {StatisticsComponent} from './Employees/pages/Register/statistics/statistics.component';
+import {RegisterPage} from './Employees/pages/Register/register-page/register-page.component';
 
-const httpLoaderFactory: (http: HttpClient) =>
-  TranslateLoader = (http: HttpClient) =>
-  new TranslateHttpLoader(http, './i18n/', '.json');
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({eventCoalescing: true}),
-    provideHttpClient(),
-    provideRouter(routes),
+// Agregar aqui los componentes
+export const routes: Routes = [
+  { path: 'home'  ,             component: HomeComponent },
+  { path: 'reports',             component: ReportsComponent },
+  { path: 'dashboards',           component: DashboardMain },
+  { path: 'dashboards/details',    component: StatisticsComponent },
+  { path: 'support',             component: SupportComponent },
+  { path: 'login',               component:LoginPage },
+  { path: 'register',             component: RegisterPage },
+];
 
-    provideTranslateService({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [HttpClient]
-      },
-      defaultLanguage: 'en',
-    })],
-};
