fcab0784246ce588f114df2e6a24002422824fa6
Gabriel Borja <borjagabriel45@gmail.com>
2025-05-16 05:24:50 -0500

    add login-page.component files

---
+++ src/app/Employees/pages/Login/components/login-page.component/login-page.component.component.ts
@@ -0,0 +1,16 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.component.html',
+  styleUrl: './login-page.component.component.css',
+  imports: [MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginPage {
+
+}
