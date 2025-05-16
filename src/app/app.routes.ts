a778a86dedf2db226a1361d20dd6ac33cd0d6bc4
S.Suarez <166082221+SDarXx@users.noreply.github.com>
2025-05-16 14:29:44 -0500

    Fix; Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,19 @@
 import { Routes } from '@angular/router';
-import {SupportComponent} from './Employees/pages/Support/components/support.component'
-import {ReportsComponent} from './Employees/pages/Reports/components/reports.component';
-import {HomeComponent} from './Employees/pages/Home/components/home.component';
-import {LoginPage} from './Employees/pages/Login/components/login-page.component/login-page.component';
-import {DashboardMain} from './Employees/pages/Dashboard/components/dashboard-main.component/dashboard-main.component';
-import {StatisticsComponent} from './Employees/pages/Register/statistics/statistics.component';
-import {RegisterPage} from './Employees/pages/Register/register-page/register-page.component';
+import { SupportComponent } from './Employees/pages/Support/components/support.component';
+import { ReportsComponent } from './Employees/pages/Reports/components/reports.component';
+import { HomeComponent } from './Employees/pages/Home/components/home.component';
+import { LoginPage } from './Employees/pages/Login/components/login-page.component/login-page.component';
+import { DashboardMain } from './Employees/pages/Dashboard/components/dashboard-main.component/dashboard-main.component';
+import { StatisticsComponent } from './Employees/pages/Register/statistics/statistics.component';
+import { RegisterPage } from './Employees/pages/Register/register-page/register-page.component';
 
-
-// Agregar aqui los componentes
 export const routes: Routes = [
-  { path: 'home'  ,             component: HomeComponent },
-  { path: 'reports',             component: ReportsComponent },
-  { path: 'dashboards',           component: DashboardMain },
-  { path: 'dashboards/details',    component: StatisticsComponent },
-  { path: 'support',             component: SupportComponent },
-  { path: 'login',               component:LoginPage },
-  { path: 'register',             component: RegisterPage },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'dashboards', component: DashboardMain },
+  { path: 'dashboards/details', component: StatisticsComponent },
+  { path: 'support', component: SupportComponent },
+  { path: 'login', component: LoginPage },
+  { path: 'register', component: RegisterPage }
 ];
-
